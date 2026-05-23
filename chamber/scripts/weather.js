f05f578160b6a3e4d22ace0360b1f50e0259bce6
hughroper <hughroper@icloud.com>
2026-05-23 17:14:00 -0600

    chamber index weather updates

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,11 +2,11 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const forecastToday = document.querySelector('today');
-const forecastTomorrow = document.querySelector('tomorrow');
-const forecastDayAfterTomorrow = document.querySelector('day-after-tomorrow');
+const forecast = document.querySelector('#forecast');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=35.8275&lon=-106.2039&units=imperial&appid=2cd88e160d4bec559b33df1aa1b21695';
+const urlForecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=35.8275&lon=-106.2039&units=imperial&appid=2cd88e160d4bec559b33df1aa1b21695';
+
 
 async function apiFetch() {
     try {
@@ -23,7 +23,24 @@ async function apiFetch() {
     }
 }
 
+async function forecastFetch() {
+    try {
+        const response = await fetch(urlForecast);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            displayForecast(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 apiFetch();
+
+forecastFetch();
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
@@ -34,3 +51,36 @@ function displayResults(data) {
     captionDesc.textContent = `${desc}`;
 }
 
+function displayForecast(data) {
+
+    const threeDayForecast = data.list.filter(item =>
+        item.dt_txt.includes('12:00:00')
+    );
+
+    for (let i = 0; i < 3; i++) {
+
+        const temp = Math.round(threeDayForecast[i].main.temp);
+        const desc = threeDayForecast[i].weather[0].description;
+        const icon = threeDayForecast[i].weather[0].icon;
+
+        const iconsrc = `https://openweathermap.org/img/w/${icon}.png`;
+
+        // Convert date string into weekday name
+        const date = new Date(threeDayForecast[i].dt_txt);
+
+        const dayName = date.toLocaleDateString('en-US', {
+            weekday: 'long'
+        });
+
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <h3>${dayName}</h3>
+            <img src="${iconsrc}" alt="${desc}">
+            <p>${temp}&deg;F</p>
+            <p>${desc}</p>
+        `;
+
+        forecast.appendChild(div);
+    }
+}
