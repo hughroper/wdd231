cdbda3b00cbba150379749c61559242e080d97b4
hughroper <hughroper@icloud.com>
2026-05-29 14:20:10 -0600

    join updates

--- chamber/scripts/join-modal.js
+++ chamber/scripts/join-modal.js
@@ -1,7 +1,35 @@
-function displayNPBenefits(benefit) {
-    courseDetails.showModal();
+const npDialog = document.querySelector("#membershipNpDetails");
+document.querySelector("#openNP").addEventListener("click", () => {
+    npDialog.showModal();
+});
+document.querySelector("#closeNP").addEventListener("click", () => {
+    npDialog.close();
+});
 
-    closeModal.addEventListener("click", () => {
-        courseDetails.close();
-    });
-}
+
+const bronzeDialog = document.querySelector("#membershipBronzeDetails");
+document.querySelector("#openBronze").addEventListener("click", () => {
+    bronzeDialog.showModal();
+});
+document.querySelector("#closeBronze").addEventListener("click", () => {
+    bronzeDialog.close();
+});
+
+
+const silverDialog = document.querySelector("#membershipSilverDetails");
+document.querySelector("#openSilver").addEventListener("click", () => {
+    silverDialog.showModal();
+});
+document.querySelector("#closeSilver").addEventListener("click", () => {
+    silverDialog.close();
+});
+
+
+const goldDialog = document.querySelector("#membershipGoldDetails");
+document.querySelector("#openGold").addEventListener("click", () => {
+    goldDialog.showModal();
+});
+document.querySelector("#closeGold").addEventListener("click", () => {
+    goldDialog.close();
+});
+
