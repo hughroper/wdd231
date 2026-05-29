fa5f9e8cef2bc7e2b9a55ea0f2a429232cfe6745
hughroper <hughroper@icloud.com>
2026-05-29 15:02:44 -0600

    join form animation updates

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -11,10 +11,13 @@ console.log(myInfo.get('email'));
 console.log(myInfo.get('phone'));
 console.log(myInfo.get('membership'));
 console.log(myInfo.get('description'));
+console.log(myInfo.get('timestamp'));
 
 document.querySelector('#results').innerHTML = `<p>Contact: ${myInfo.get('firstname')} ${myInfo.get('lastname')}</p>
 <p>Email: <a href="mailto:${myInfo.get('email')}">${myInfo.get('email')}</a></p>
 <p>Phone: ${myInfo.get('phone')}</p>
 <p>Organization: ${myInfo.get('organization')}</p>
 <p>Level: ${myInfo.get('membership')}</p>
-<p>Description: ${myInfo.get('description')}</p>`;
+<p>Description: ${myInfo.get('description')}</p>
+<p>Timestamp: ${myInfo.get('timestamp')}</p>
+`;
