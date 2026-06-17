9863d0d402bdf83c5d238007b73be2032929cba0
hughroper <hughroper@icloud.com>
2026-06-16 20:23:52 -0600

    harp more stuff

--- finalproject/scripts/form.js
+++ finalproject/scripts/form.js
@@ -1,3 +1,46 @@
+const form = document.querySelector("#waitListForm");
+const dialog = document.querySelector("#confirmDialog");
+
+const editButton = document.querySelector("#editButton");
+const confirmButton = document.querySelector("#confirmButton");
+
 document.querySelector("#timestamp").value =
     new Date().toLocaleString();
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    document.querySelector("#confirmFirstName").textContent =
+        document.querySelector("#firstname").value;
+
+    document.querySelector("#confirmLastName").textContent =
+        document.querySelector("#lastname").value;
+
+    document.querySelector("#confirmEmail").textContent =
+        document.querySelector("#email").value;
+
+    document.querySelector("#confirmPhone").textContent =
+        document.querySelector("#phone").value;
+
+    document.querySelector("#confirmAge").textContent =
+        document.querySelector("#age").value;
+
+    document.querySelector("#confirmExperience").textContent =
+        document.querySelector('input[name="experience"]:checked').value;
+
+    document.querySelector("#confirmInterest").textContent =
+        document.querySelector('input[name="interest"]:checked').value;
+
+    document.querySelector("#confirmQuestions").textContent =
+        document.querySelector("#question").value;
+
+    dialog.showModal();
+});
+
+editButton.addEventListener("click", () => {
+    dialog.close();
+});
+
+confirmButton.addEventListener("click", () => {
+    form.submit();
+});
